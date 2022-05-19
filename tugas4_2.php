7708c29b416f951aa72796a7da2b7eb0c2168f90
mohfarhann <90471536+mohfarhann@users.noreply.github.com>
2022-05-19 14:07:06 +0700

    Add files via upload

---
+++ tugas4_2.php
@@ -0,0 +1,13 @@
+<?php
+  $x = 523;
+  $y = 8891;
+  $z = $y / $x;
+
+
+
+  echo "Jumlah yang Saluran = $x";
+  echo "</br> Jumlah yang Terpakai = $y";
+  echo "</br> Rata-Rata Pemakaian = $z";
+  echo "</br>Jawaban: $z liter/keluarga";
+
+ ?>
